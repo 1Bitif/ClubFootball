e7ce99260139e26ffd3b1ee79620b4e4065c586c
1Bitif <aajliabdellatif@gmail.com>
2025-05-25 22:33:31 +0100

    repiared component Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Hero } from '../../components/Sections/Hero/Hero'
 import { About } from '../../components/Sections/About/About'
+import { motion } from 'framer-motion'
 
 export const Home = () => {
+  const aboutRef = useRef(null)
+
+  const scrollToAbout = () => {
+    aboutRef.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
-    <main className=''>
-        <Hero />
+    <motion.main 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className='overflow-x-hidden'
+    >
+      <Hero scrollToAbout={scrollToAbout} />
+      <div ref={aboutRef}>
         <About />
-    </main>
+      </div>
+    </motion.main>
   )
 }
